2767471d68068b6bbc7845882e087413e1f1c883
Rekha Remadevi <50706192+rekharchandran@users.noreply.github.com>
2020-09-10 14:28:34 +0200

    Add files via upload

---
+++ Number Swap.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+// A program to print the output of multiplication of three numbers which will be entered by the user
+
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number to multiply");
+
+            int firstNumber = 2;
+
+            Console.Write("Input the second number to multiply");
+
+            int secondNumber = 3;
+
+
+            Console.Write("Input the third number to multiply");
+
+            int thirdNumber = 6;
+
+            int product = firstNumber * secondNumber * thirdNumber;
+
+            Console.WriteLine(product);            
+
+
+
+
+
+        }
+    }
+}
